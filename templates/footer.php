72160055f08d87993dc901d40ba569063bc62c36
João Pedro Sassi Granado <joao.granado@fatec.sp.gov.br>
2021-11-26 13:57:44 -0300

    Upt nome

--- templates/footer.php
+++ templates/footer.php
@@ -1,5 +1,5 @@
    <footer>
-      <p>Pizzaria do João &copy; 2021</p>
+      <p>Pizzaria do Franchesco &copy; 2021</p>
    </footer>
    <!--Bootstrap JS-->
    <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
